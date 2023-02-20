012e2da52a516de2b16c76d2281db095e64dbaa6
Fenr1sulfr <120082611+Fenr1sulfr@users.noreply.github.com>
2023-02-21 01:00:33 +0600

    Update Main.java

--- Assigment-3/src/Main.java
+++ Assigment-3/src/Main.java
@@ -1,5 +1,8 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.sql.*;
 import java.util.Scanner;
+import java.util.logging.SocketHandler;
 
 public class Main {
     private static final String url = "jdbc:mysql://localhost:3306/market";
@@ -11,7 +14,7 @@ public class Main {
     private static ResultSet rs;
     public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Who are you?\n1. User\n2. Admin\n3. Seller");
+        System.out.println("Who are you?\n1. Buyer\n2. Admin\n3. Seller\n4)Buyer");
         int roleChoice = sc.nextInt();
 
         switch (roleChoice) {
@@ -19,6 +22,17 @@ public class Main {
                 System.out.println("You are entry as Buyer");
                 Buyer buyer=new Buyer();
                 buyer.getFromDatabase();
+                System.out.println("What do you want to buy?\n1)Cold\n2)Fire\n3)Nuclear");
+                int choice=sc.nextInt();
+                if(choice==1){
+                    buyer.buyColdWeapon();
+                }
+                if(choice==2){
+                    buyer.buyFireWeapon();
+                }
+                if(choice==3){
+                    buyer.buyNuclearWeapon();
+                }
                 break;
             case 2:
                 System.out.println("Special question for admin: What's the capital of France?");
@@ -36,7 +50,6 @@ public class Main {
                 System.out.println("You are entry as Seller");
                 Seller seller=new Seller();
                 seller.sendToDatabase();
-
             default:
                 System.out.println("Invalid option.");
         }
